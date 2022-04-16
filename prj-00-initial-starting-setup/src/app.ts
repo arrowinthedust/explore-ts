858223629eca1ceb328d7b88a795fca556365374
Vikas Bhardwaj <arrowinthedust@gmail.com>
2022-04-16 14:28:13 +1000

    basic validation

--- prj-00-initial-starting-setup/src/app.ts
+++ prj-00-initial-starting-setup/src/app.ts
@@ -43,8 +43,42 @@ class ProjectInput {
     private submitHandler(event: Event) {
         event.preventDefault();
         console.log(this.titleInputElement.value);
+        const userInput = this.gatherUserInput();
+
+        if(Array.isArray(userInput)){
+            const [title, desc, people] = userInput;
+            console.log(title, desc, people);
+        }
+
+        this.clearInputs(); 
 
     }
+
+    private clearInputs() {
+        this.titleInputElement.value = '';
+        this.descriptionInputElement.value = '';
+        this.peopleInputElement.value = '';
+    }
+
+    private gatherUserInput():[string,string, number] | void {
+        const enteredTitle = this.titleInputElement.value;
+        const enteredDescription = this.descriptionInputElement.value;
+        const enteredPeople = this.peopleInputElement.value;
+    
+        if(
+            enteredTitle.trim().length === 0 ||
+            enteredDescription.trim().length === 0 ||
+            enteredPeople.trim().length ===0
+        ) {
+            console.log('alert');
+            alert('Invalid Input');
+            return;
+        }else{
+            return [enteredTitle, enteredDescription, +enteredPeople];
+        }
+    }
+
+
     private configure() {
         this.element.addEventListener('submit', this.submitHandler);
     }
