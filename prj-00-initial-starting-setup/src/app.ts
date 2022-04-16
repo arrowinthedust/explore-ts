6ea9ff4b7e328b72e2dd18433cc500b1df500ac3
Vikas Bhardwaj <arrowinthedust@gmail.com>
2022-04-16 17:01:32 +1000

    Project listing without state management

--- prj-00-initial-starting-setup/src/app.ts
+++ prj-00-initial-starting-setup/src/app.ts
@@ -52,8 +52,41 @@ function autobind(_1: any, _2: string, descriptor: PropertyDescriptor) {
     return adjDescriptor;
 
 }
-
 //end autobind decorator
+
+// Project List
+class ProjectList{
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    element: HTMLElement;
+
+    constructor(private type: 'active' |'finished' ) {
+        this.templateElement = document.getElementById('project-list')! as HTMLTemplateElement;
+        this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild as HTMLFormElement;
+        this.element.id = `${this.type}-projects`;
+
+        this.attach();
+        this.renderContent();
+
+    }
+
+    private attach() {
+        this.hostElement.insertAdjacentElement("beforeend",this.element);
+    }
+
+    private renderContent(){
+        const listId = `${this.type}-projects-list`
+        this.element.querySelector('ul')!.id = listId;
+        this.element.querySelector('h2')!.textContent = this.type.toUpperCase() + ' PROJECTS';
+    }
+}
+
+
+// end project List
+
 class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
@@ -141,3 +174,5 @@ class ProjectInput {
 }
 
 const prjInput = new ProjectInput();
+const activeProjectList = new ProjectList('active');
+const finishedProjectList = new ProjectList('finished');
